e7248f1b59229d6ad005a41479cbfe64cccc590d
Sanjay Balaji <sanjay.balaji@oracle.com>
2020-09-09 14:20:56 +0530

    testing

---
+++ app.py
@@ -0,0 +1,26 @@
+import json
+from flask import Flask, render_template, request, jsonify, make_response
+from main.parser import *
+
+# init flask app
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def home():
+    return "Hey, I am a URL Shortener Service."
+
+@app.route('/shorten', methods=['POST'])
+def shorten():
+    if not request.is_json():
+        print("Hey! This ain't a JSON.")
+        return make_response(jsonify({'error': 'Bad Request'}), 400)
+    else:
+        print("Processing...")
+        encoded_url={ 'data': insertUrl(request.json.incoming_url)}
+        print("Complete.")
+        return jsonify(encoded_url)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ main/parser.py
@@ -0,0 +1,43 @@
+import os
+import collections
+import redis
+
+counter = collections.Counter(range(1, 100))
+
+def encodeUrl(self, id):
+    """
+        Encodes and returns a base62 output
+    """
+    characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    # base = 62
+    base = len(characters)
+    ret = []
+    while id > 0:
+        val = id % base
+        ret.append(characters[val])
+        id = id // base
+    # reverse and return
+    return "".join(ret[::-1])
+
+def dbConnect():
+    """
+        Attempts a DB connection and returns the DB Object
+    """
+    r = 0
+    try:
+        r = redis.from_url(os.environ.get("REDIS_URL"))
+    except Exception as error:
+        print ("Oops! An exception has occured:", error)
+        print ("Exception TYPE:", type(error))
+        r = None
+    finally:
+        return r
+
+def insertUrl(incoming_url):
+    r = dbConnect()
+    if incoming_url in r:
+        return r.get(incoming_url)
+    else:
+        encoded_url = encodeUrl(incoming_url, counter.get())
+        r.set(incoming_url, encoded_url)
+        return encoded_url
